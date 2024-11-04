98477f30816b88b59a7a3d716814b8fde6e3a281
RahulReddy <krahulreddy45@gmail.com>
2024-11-04 15:18:55 +0100

    Initial Commit

---
+++ new proj/new_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_projApp.swift
+//  new proj
+//
+//  Created by Rahul Reddy Karri on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct new_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
